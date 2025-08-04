48d590d0b7a0fa0bc2cdd7fea983369269826c83
nauan2024 <nauanlaurentinotec@gmail.com>
2025-08-04 16:42:17 -0300

    "feat: executei alguns testes"

---
+++ js/hello.js
@@ -0,0 +1,1 @@
+console.log("ISSO É UM TESTE")
---
+++ js/variaveis.js
@@ -0,0 +1,47 @@
+// tipo nome = valor
+let nauan = 44
+
+
+// var cidade = "pernambucas"
+
+// console.log(cidade)
+
+// console.log("a cidade" + cidade)
+
+// console.log(`A cidade é:${cidade}`)
+
+// var idade = 20
+
+
+// console.log(idade)
+
+// let nome = "matheus"
+
+// nome = "lucas"
+
+// console.log(nome)
+
+// const readline = require("readline-sync")
+
+// const nome = readline.question("QUAL É O SEU NOME?")
+
+// console.log(nome)
+
+// console.log("Olá! esse é meu segundo comando JavaScript")
+
+// console.log(2+2)
+
+// const anoNascimento = 2007
+// anoNascimento = 1990
+
+// console.log(anoNascimento)
+
+let nome = "thomas"
+
+let sobrenome = "dores de jesus"
+
+
+console.log("boa tarde" )
+console.log(`meu nome é ${nome} ${sobrenome}`)
+console.log("me chame de senhor " + sobrenome) 
+
